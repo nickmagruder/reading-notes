dcafe0ba44993be6c9cc2d5625656f1581d70a6e
nickmagruder <nick.magruder@outlook.com>
2020-11-29 16:28:20 -0800

    code 301 notes  skeleton built

---
+++ miscjs.js
@@ -0,0 +1,15 @@
+function Graph() {
+    this.nodes = [];
+    this.edges = [];
+  }
+  
+  Graph.prototype.addNodes = function(n){
+    this.nodes.push(n);
+  };
+  
+  Graph.prototype.addEdges = function(n){
+    this.edges.push(n);
+  };
+  
+  var graphObj = new Graph();
+  
